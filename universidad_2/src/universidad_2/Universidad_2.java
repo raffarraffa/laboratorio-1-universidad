fef5248dfad78f38d6e2b0009bdd1c755aa0ae71
Toloza  Leonel <112735110+LeoToloza1@users.noreply.github.com>
2023-05-20 21:03:20 -0300

    Add Views - home 20/5
    Se agrega la carpeta views con los siguientes formularios

--- universidad_2/src/universidad_2/Universidad_2.java
+++ universidad_2/src/universidad_2/Universidad_2.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import universidad_2.controllers.AlumnoData;
 import universidad_2.controllers.Conexion;
+import universidad_2.controllers.InscripcionData;
 import universidad_2.controllers.MateriaData;
 import universidad_2.models.Alumno;
+import universidad_2.models.Inscripcion;
 import universidad_2.models.Materia;
 
 /**
@@ -16,8 +19,32 @@ import universidad_2.models.Materia;
 public class Universidad_2 {
 
     public static void main(String[] args) throws IOException, SQLException {
-        Conexion.getConnection();
+        Conexion.getConnection(); // llama conexion
+        // instancias DATA
+        MateriaData materia_data = new MateriaData();
+        MateriaData data_materia = new MateriaData();
+        AlumnoData data_alumno = new AlumnoData();
 
-   
+        Materia materia = new Materia();
+        materia = materia_data.selectMateria("Ingles");
+
+        Alumno alumno = new Alumno();
+        alumno = data_alumno.selectAlumnoDni("43690464");
+        InscripcionData inscripcion_materia = new InscripcionData();
+        //    inscripcion_materia.insertInscripcion(alumno, materia);
+
+        ArrayList<Materia> datos_materia = data_materia.selectMaterias(1);
+        for (Materia mat : datos_materia) {
+            System.out.println(mat.toString());
+        }
+        // consulta alumnos inscriptos  en una materia
+        //  AlumnoData data_alumno = new AlumnoData();
+        ArrayList<Alumno> datos_alumnos = data_alumno.selectAlumnosMateria(1);
+        for (Alumno alu : datos_alumnos) {
+            System.out.println(alu.toString());
+        }
+        System.out.println(alumno);
+//        System.out.println(alumno.toString());
+
     }
 }
---
+++ universidad_2/src/universidad_2/models/Inscripcion.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidad_2.models;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author raffarraffa
+ */
+public class Inscripcion {
+
+    private int id_inscripto;
+    private double nota;
+    private Alumno alumno;
+    private Materia materia;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int id_inscripto, double nota, Alumno alumno, Materia materia) {
+        this.id_inscripto = id_inscripto;
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Inscripcion(double nota, Alumno alumno, Materia materia) {
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public int getId_inscripto() {
+        return id_inscripto;
+    }
+
+    public void setId_inscripto(int id_inscripto) {
+        this.id_inscripto = id_inscripto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+}
